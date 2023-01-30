1ff5465f684becf62d019eb7a34aae4e28b740bf
Scionzenos <73364702+Scionzenos@users.noreply.github.com>
2023-01-29 23:45:20 -0500

    Initial Commit

---
+++ ConstraintCopier.cs
@@ -0,0 +1,56 @@
+﻿#if(UNITY_EDITOR)
+using UnityEngine;
+#pragma warning disable IDE0090 // Use 'new(...)'
+public class ConstraintCopier : MonoBehaviour
+{
+    [Tooltip("The root of the chain of bones that you want to constrain to something else")]
+    public GameObject sourceRoot;
+    [Tooltip("The root of the chain of bones that you want something else to be constrained to")]
+    public GameObject targetRoot;
+    [Tooltip("Which bones in the hierarchy that you want the script to stop when it hits")]
+    public Transform[] ignoreTransforms;
+    public int constraintType;
+
+    private UnityEngine.Animations.ParentConstraint parentConstraint;
+    private UnityEngine.Animations.RotationConstraint rotationConstraint;
+    private UnityEngine.Animations.PositionConstraint positionConstraint;
+
+    protected void OnDrawGizmosSelected()
+    {
+        //Gizmos.DrawCube(new Vector3(0, stepHeight, 0), new Vector3(3, 0.0001f, 3));
+    }
+    public void CreateConstraintRelationship(int constraintType)
+    {
+        CreateConstraintAtBone(this.sourceRoot, this.targetRoot, constraintType);
+    }
+
+    private void CreateConstraintAtBone(GameObject sourceRoot, GameObject targetRoot,int constraintType)
+    {
+        if (constraintType == 0) // Parent Constraint
+        {
+            parentConstraint = sourceRoot.AddComponent<UnityEngine.Animations.ParentConstraint>();
+            parentConstraint.AddSource(new UnityEngine.Animations.ConstraintSource { weight = 1, sourceTransform = targetRoot.transform });
+            parentConstraint.constraintActive = true;
+            parentConstraint.locked = true;
+        }
+        else if (constraintType == 1) // Rotation Constraint
+        {
+            rotationConstraint = sourceRoot.AddComponent<UnityEngine.Animations.RotationConstraint>();
+            rotationConstraint.AddSource(new UnityEngine.Animations.ConstraintSource { weight = 1, sourceTransform = targetRoot.transform });
+            rotationConstraint.constraintActive = true;
+            rotationConstraint.locked = true;
+        }
+        else // Position constraint
+        {
+            positionConstraint = sourceRoot.AddComponent<UnityEngine.Animations.PositionConstraint>();
+            positionConstraint.AddSource(new UnityEngine.Animations.ConstraintSource { weight = 1, sourceTransform = targetRoot.transform });
+            positionConstraint.constraintActive = true;
+            positionConstraint.locked = true;
+        }
+        for (int i = 0; i < sourceRoot.transform.childCount; i++)
+        {
+            CreateConstraintAtBone(sourceRoot.transform.GetChild(i).gameObject, targetRoot.transform.GetChild(i).gameObject, constraintType);
+        }
+    }
+}
+#endif 
---
+++ Editor/ConstraintCopierCustomInspector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+#pragma warning disable IDE0090 // Use 'new(...)'
+[CustomEditor(typeof(ConstraintCopier))]
+public class RotationConstraintCopyCustomInspector : Editor 
+{
+    int constraintType;
+    string[] constraintTypeArray = { "Parent Constraint", "Rotation Constraint", "Position Constraint" };
+    public override void OnInspectorGUI()
+    {
+        constraintType = GUILayout.Toolbar(constraintType, constraintTypeArray);
+        ConstraintCopier script = (ConstraintCopier)target;
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("sourceRoot"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("targetRoot"));
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUILayout.Button("Create Constraints"))
+        {
+            script.CreateConstraintRelationship(constraintType);
+        }
+    }
+
+    [MenuItem("Tools/Scionzenos/Constraint Copier")]
+    public static void EnableConstraintCopier()
+    {
+        GameObject setupGameobject = new GameObject("Constraint Copier");
+        setupGameobject.AddComponent<ConstraintCopier>();
+    }
+}
